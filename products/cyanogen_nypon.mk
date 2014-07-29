a75bfcb898f682e65c01a1a9e6b6dbbd85831961
Gava97 <gava97@gmail.com>
2014-07-29 20:39:52 +0200

    typo

--- products/cyanogen_nypon.mk
+++ products/cyanogen_nypon.mk
@@ -1,10 +1,3 @@
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := nypon
-PRODUCT_DEVICE := nypon
-PRODUCT_BRAND := Sony
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_MODEL := Xperia P
-
 
 # Inherit AOSP device configuration for nypon.
 $(call inherit-product, device/sony/nypon/full_nypon.mk)
@@ -26,7 +19,7 @@ PRODUCT_MANUFACTURER := Sony
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST25i_1262-1120 BUILD_FINGERPRINT=SEMC/ST25i_1262-1120/ST25i:4.0.4/6.1.1.B.1.10/9bt3zw:user/release-keys PRIVATE_BUILD_DESC="ST25i-user 4.0.4 6.1.1.B.1.10 9bt3zw test-keys"
 
 # Extra nypon overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/nypom
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/nypon
 # Add the Torch app
 PRODUCT_PACKAGES += Torch
 
